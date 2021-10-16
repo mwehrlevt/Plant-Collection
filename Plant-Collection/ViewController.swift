cba3bb5aa234442bfc51d5811c9179345370183d
Michelle <mwehrlevt@gmail.com>
2021-10-16 16:54:48 -0400

    Initial Commit

---
+++ Plant-Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Plant-Collection
+//
+//  Created by Michelle on 10/16/21.
+//  Copyright © 2021 mwehrle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Plant-CollectionTests/Plant_CollectionTests.swift
@@ -0,0 +1,34 @@
+//
+//  Plant_CollectionTests.swift
+//  Plant-CollectionTests
+//
+//  Created by Michelle on 10/16/21.
+//  Copyright © 2021 mwehrle. All rights reserved.
+//
+
+import XCTest
+@testable import Plant_Collection
+
+class Plant_CollectionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
