05073dd9d896d3e2d463732a385c306a9965c734
Michelle <mwehrlevt@gmail.com>
2021-10-20 13:40:05 -0400

    Got table cells to show up

--- Plant-Collection/ItemsViewController.swift
+++ Plant-Collection/ItemsViewController.swift
@@ -11,8 +11,12 @@ import UIKit
 class ItemsViewController: UITableViewController {
     var plantItemsStore: PlantItemsStore!
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return plantItemsStore.allPlantItems.count
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
         // Set the tect on the cell
         let plant = plantItemsStore.allPlantItems[indexPath.row]
